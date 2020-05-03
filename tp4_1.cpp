711ec5ff5730f3c2cc89285ac251959c0532b6f1
Guido D'Arpino <gldarpino@gmail.com>
2020-05-03 01:45:15 -0300

    Add. Añado tp4_1.cpp resuelto hasta el punto 4

---
+++ tp4_1.cpp
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void vaciar_buffer();
+struct Tarea {
+int TareaID; //Numerado en ciclo iterativo
+char* Descripcion ; //
+int Duracion; // entre 10 – 100
+};
+void mostrarTarea(Tarea);
+
+void cargarRealizadas(Tarea ** pVector,Tarea ** pRealizadas,int cant){
+    
+    for (int i = 0; i < cant; i++)
+    {
+        mostrarTarea(*pVector[i]);
+        bool flag=false;
+        do
+        {
+            flag=false;
+            printf("Ya completó esta tarea? (S/n)\n");    
+            char resp;
+            
+            vaciar_buffer();  //El fflush No funciona (ni está indicado) para borrar el búfer de entrada, así que defino esta funcion
+            scanf("%c",&resp);
+            if (resp=='s' || resp=='S')
+            {
+                pRealizadas[i]=pVector[i];
+                pVector[i]=NULL;
+                //Borrar de pendientes y pasar a realizados
+            }
+            else if (!(resp=='n' || resp=='N')){
+                flag=true;
+                printf("Respuesta inválida, intente de nuevo\n");
+            }                           
+        } while (flag);
+        
+    }
+}
+
+void cargarTareas(Tarea ** pVector,int cant){
+    for (int i = 0; i < cant; i++)
+    {
+        char s[100];//String auxiliar
+        pVector[i]=(Tarea *) malloc(sizeof(Tarea*));
+        (*pVector[i]).TareaID=i;
+        printf("Ingrese la descripción de la tarea: ");
+
+        scanf("%s",s);
+        
+        (*pVector[i]).Descripcion=(char *)malloc(sizeof(s)*strlen(s));
+        strcpy(((*pVector[i]).Descripcion),s);
+
+        int duracion=rand()%101+10;
+        (*pVector[i]).Duracion=duracion;
+    }
+    
+}
+
+void mostrarTarea(Tarea t){
+printf("\nID de la Tarea: %d\n"\
+                "Descripción: %s\n"\
+                "Duración: %d\n",
+                t.TareaID,t.Descripcion,
+                t.Duracion);
+}
+
+void mostrarTareas(Tarea ** pVector,int cant){
+    for (int i = 0; i < cant; i++)
+        if (pVector[i]!=NULL)   
+        {
+            mostrarTarea(*pVector[i]);
+        }        
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+    printf("Cuántas tareas va a cargar?: ");
+    int cantTareas;
+    scanf("%d", &cantTareas);
+    Tarea **pTareas;
+    pTareas = (Tarea **)malloc(sizeof(Tarea*)*cantTareas);
+    cargarTareas(pTareas,cantTareas);
+    Tarea **pTareasRealizadas = (Tarea **)malloc(sizeof(Tarea*)*cantTareas);;
+    cargarRealizadas(pTareas,pTareasRealizadas,cantTareas);
+
+    printf("\n\n\tTareas Pendientes\n");
+    mostrarTareas(pTareas,cantTareas);
+
+    printf("\n\tTareas Realizadas\n");
+    mostrarTareas(pTareasRealizadas,cantTareas);
+
+    free(pTareas);
+    free(pTareasRealizadas);
+    getchar();
+    return 0;
+}
+
+
+
+void vaciar_buffer()
+{
+  int ch;
+  while ((ch = getchar()) != '\n' && ch != EOF);
+}
